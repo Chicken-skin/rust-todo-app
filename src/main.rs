0a5d0d0e527cad83513991f86eb6ad643889ff25
森山大河 <tiger47@MacBookAir.local>
2024-03-11 21:12:30 +0900

    add comment

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let app = Router::new().route("/", get(root)); // ルーティング
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000)); // 127.0.0.1:3000 (localhost:3000)
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
 
     axum::serve(listener, app.into_make_service())
-        .await
+        .await // 非同期タスクはawaitされるまで実行されない
         .unwrap();
 }
 
