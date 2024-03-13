b5187173adf1933a4a499faaa931e6fcba02ed7a
Chicken-skin <tiger47@MacBookAir.local>
2024-03-13 20:47:13 +0900

    delete old code

--- src/main.rs
+++ src/main.rs
@@ -149,50 +149,11 @@ async fn root() -> &'static str {
     "Hello, world!"
 }
 
-/// # create_user
-/// This function create user and return response.
-///
-/// ## Arguments
-/// * Json data of CreateUser struct.
-///
-/// ## Return
-/// * Something it is impl IntoResponse trait
-async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // ここでSerialize
-    // http status codeはCREATED(201)
-    // response BodyはuserをJSON Serializeしたものをレスポンスに含める
-    (StatusCode::CREATED, Json(user)) // StatusCodeのみを返すこともできる
-}
-
-/// # CreateUser
-/// This struct is used for request.
-#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
-struct CreateUser {
-    username: String,
-}
-
-/// # User
-/// This struct is used for response.
-#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
-struct User {
-    id: u64,
-    username: String,
-}
-
 // test
 #[cfg(test)]
 mod test {
     use super::*;
-    use axum::{
-        body::{to_bytes, Body, Bytes},
-        http::{header, Method, Request},
-    };
-    use hyper::body;
+    use axum::{body::Body, http::Request};
     use tower::ServiceExt;
 
     // root関数のtest
@@ -206,31 +167,5 @@ mod test {
         let body: String = String::from_utf8(bytes.to_vec()).unwrap();
 
         assert_eq!(body, "Hello, world!");
-    }
-
-    // JSON bodyをtest
-    #[tokio::test]
-    async fn should_return_user_data() {
-        // request作成
-        let repository = TodoRepositoryForMemory::new();
-        let req = Request::builder()
-            .uri("/users")
-            .method(Method::POST)
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(Body::from(r#"{ "username": "Phil Foden" }"#))
-            .unwrap();
-        let res = create_app(repository).oneshot(req).await.unwrap();
-
-        let bytes = axum::body::to_bytes(res.into_body(), 128).await.unwrap();
-        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
-        let user: User = serde_json::from_str(&body).expect("cannot convert User instance.");
-        // UserがPartialEqを実装しているので比較可能.
-        assert_eq!(
-            user,
-            User {
-                id: 1337,
-                username: "Phil Foden".to_string(),
-            }
-        );
     }
 }
