e6a0bc5dca14ee25af86a5d106d810985f3ca1a0
Chicken-skin <tiger47@MacBookAir.local>
2024-03-18 20:54:34 +0900

    fix repositories and handlers for async

--- src/repositories.rs
+++ src/repositories.rs
@@ -4,6 +4,7 @@ use std::{
 };
 
 use anyhow::Context;
+use axum::async_trait;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use validator::{self, Validate};
@@ -14,12 +15,13 @@ enum RepositoryError {
     NotFound(i32),
 }
 
+#[async_trait]
 pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
-    fn create(&self, payload: CreateTodo) -> Todo;
-    fn find(&self, id: i32) -> Option<Todo>;
-    fn all(&self) -> Vec<Todo>;
-    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo>;
-    fn delete(&self, id: i32) -> anyhow::Result<()>;
+    async fn create(&self, payload: CreateTodo) -> anyhow::Result<Todo>;
+    async fn find(&self, id: i32) -> anyhow::Result<Todo>;
+    async fn all(&self) -> anyhow::Result<Vec<Todo>>;
+    async fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo>;
+    async fn delete(&self, id: i32) -> anyhow::Result<()>;
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
@@ -92,26 +94,31 @@ impl TodoRepositoryForMemory {
     }
 }
 
+#[async_trait]
 impl TodoRepository for TodoRepositoryForMemory {
-    fn create(&self, payload: CreateTodo) -> Todo {
+    async fn create(&self, payload: CreateTodo) -> anyhow::Result<Todo> {
         let mut store = self.write_store_ref(); // スレッドセーフな書き込み権限ありHashMap
         let id = (store.len() + 1) as i32; // HashMapの長さ+1をidにする(i32)
         let todo = Todo::new(id, payload.text.clone()); // Todoインスタンスを新しく作成
         store.insert(id, todo.clone()); // store(HashMap)に追加
-        todo // Todoを返すことで、作成されたtodoのidやインスタンスを知れる
+        Ok(todo) // Todoを返すことで、作成されたtodoのidやインスタンスを知れる
     }
 
-    fn find(&self, id: i32) -> Option<Todo> {
+    async fn find(&self, id: i32) -> anyhow::Result<Todo> {
         let store = self.read_store_ref(); // read権限のあるstore
-        store.get(&id).map(|todo| todo.clone()) // 指定されたidをgetして,そのcloneを返す
+        let todo = store
+            .get(&id)
+            .map(|todo| todo.clone()) // 指定されたidをgetして,そのcloneを返す
+            .ok_or(RepositoryError::NotFound(id))?;
+        Ok(todo)
     }
 
-    fn all(&self) -> Vec<Todo> {
+    async fn all(&self) -> anyhow::Result<Vec<Todo>> {
         let store = self.read_store_ref(); // read権限のあるstore
-        Vec::from_iter(store.values().map(|todo| todo.clone())) // storeの全データをクローンしたVector
+        Ok(Vec::from_iter(store.values().map(|todo| todo.clone()))) // storeの全データをクローンしたVector
     }
 
-    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
+    async fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
         let mut store = self.write_store_ref(); // read権限のあるstore
         let todo = store.get(&id).context(RepositoryError::NotFound(id))?; // idnの値をget. なければNotFoundエラー
         let text = payload.text.unwrap_or(todo.text.clone()); // 引数のtext. なければtodoのtextのclone
@@ -126,7 +133,7 @@ impl TodoRepository for TodoRepositoryForMemory {
         Ok(todo) // 成功したらOkで新しいtodoを返す
     }
 
-    fn delete(&self, id: i32) -> anyhow::Result<()> {
+    async fn delete(&self, id: i32) -> anyhow::Result<()> {
         let mut store = self.write_store_ref(); // 書き込み権限ありsotre
         store.remove(&id).ok_or(RepositoryError::NotFound(id))?; // idのデータがあればremove
         Ok(()) // 成功すればOkを返す
@@ -137,23 +144,26 @@ impl TodoRepository for TodoRepositoryForMemory {
 mod test {
     use super::*;
 
-    #[test]
-    fn todo_crud_scenario() {
+    #[tokio::test]
+    async fn todo_crud_scenario() {
         let text = "todo text".to_string();
         let id = 1;
         let expected = Todo::new(id, text.clone());
 
         // create
         let repository = TodoRepositoryForMemory::new();
-        let todo = repository.create(CreateTodo { text });
+        let todo = repository
+            .create(CreateTodo { text })
+            .await
+            .expect("failed create todo");
         assert_eq!(expected, todo);
 
         // find
-        let todo = repository.find(todo.id).unwrap();
+        let todo = repository.find(todo.id).await.unwrap();
         assert_eq!(expected, todo);
 
         // all
-        let todo = repository.all();
+        let todo = repository.all().await.expect("failed get all todos");
         assert_eq!(vec![expected], todo);
 
         // update
@@ -166,6 +176,7 @@ mod test {
                     completed: Some(true),
                 },
             )
+            .await
             .expect("failed update todo");
 
         assert_eq!(
@@ -178,7 +189,7 @@ mod test {
         );
 
         // delete
-        let res = repository.delete(id);
+        let res = repository.delete(id).await;
         assert!(res.is_ok());
     }
 }
